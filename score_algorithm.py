9287dfeb1fb724b4c40828242ea91d640ec2a015
wyllow7 <will94539@gmail.com>
2026-03-12 12:32:33 -0700

    estimated max distance, added to need to do list
    will need coordinate system to get accurate max distance

--- score_algorithm.py
+++ score_algorithm.py
@@ -6,21 +6,23 @@ Requires:
         - Requires scaled map with coordinate system: not implemented yet
     - Score formula from geogeussr: 5000 * e^(-10 * (Distance / Max Distance))
         - From "Beginner's Guide to Geoguessr", might be helpful later: https://www.plonkit.net/beginners-guide#game-mechanics
-            - "'Distance' naturally refers to the distance between the guess and the correct location. 'Max Distance' is the diagonal of the smallest rectangle possible that would contain every location in the map."
+            - "'Distance' naturally refers to the distance between the guess and the correct location. 
+               'Max Distance' is the diagonal of the smallest rectangle possible that would contain every location in the map."
 
 Still need to do:
     - Input from frontend for guess_point and correct_point
     - Calculate max distance for sjsu campus
+        - Will need coordinate system ready
 """
 
 import math
 
 def score_algorithm():
-    MAX_DISTANCE = 100 # diagonal of smallest rectangle possible that contains every location on map, ie. rectangle that surrounds sjsu campus
+    MAX_DISTANCE = 1000 # diagonal of smallest rectangle possible that contains every location on map, ie. rectangle that surrounds sjsu campus
 
     # calculate distance
     guess_point = [10, 0] # need to get values from user input/stored values for correct locations later
-    correct_point = [10, 1]
+    correct_point = [10, 150]
 
     distance = math.dist(guess_point, correct_point) # distance between guess and correct location
         # previous distance formula in case math function doesnt work, need individual coordinates:
