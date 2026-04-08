c20745345ef32f6c51fe8f276c94fe2ca0395f50
Paracosms <ahiponia@protonmail.com>
2026-04-07 18:22:39 -0700

    routing fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/spartanguessr/',
+  base: '/apps/spartanguessr/',
   plugins: [react()],
 })
