bd3492d1d3d2c5d04e68c589be1d55e0d3ea20f6
wyllow7 <will94539@gmail.com>
2026-04-12 16:04:08 -0700

    adjusted score algorithm to be more accurate

--- backend/score_algorithm.py
+++ backend/score_algorithm.py
@@ -1,24 +1,16 @@
 """
 SpartanGuessr Score Algorithm
 
-Requires:
-    - Distance calculator: use distance formula
-        - Requires scaled map with coordinate system: not implemented yet
-    - Score formula from geogeussr: 5000 * e^(-10 * (Distance / Max Distance))
-        - From "Beginner's Guide to Geoguessr", might be helpful later: https://www.plonkit.net/beginners-guide#game-mechanics
-            - "'Distance' naturally refers to the distance between the guess and the correct location. 
-               'Max Distance' is the diagonal of the smallest rectangle possible that would contain every location in the map."
-
-Still need to do:
-    - Input from frontend for guess_point and correct_point
-    - Calculate max distance for sjsu campus
-        - Will need coordinate system ready
+- Score formula from geogeussr: 5000 * e^(-10 * (Distance / Max Distance))
+    - From "Beginner's Guide to Geoguessr", might be helpful later: https://www.plonkit.net/beginners-guide#game-mechanics
+        - "'Distance' naturally refers to the distance between the guess and the correct location. 
+           'Max Distance' is the diagonal of the smallest rectangle possible that would contain every location in the map."
 """
 
 import math
 
 def score_algorithm(guess_point, correct_point):
-    MAX_DISTANCE = 1000 # diagonal of smallest rectangle possible that contains every location on map, ie. rectangle that surrounds sjsu campus
+    MAX_DISTANCE = 2073 # diagonal of smallest rectangle possible that contains every location on map, ie. rectangle that surrounds sjsu campus
 
     distance = math.dist(guess_point, correct_point) # distance between guess and correct location
         # previous distance formula in case math function doesnt work, need individual coordinates:
