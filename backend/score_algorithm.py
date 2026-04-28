ad76a7b735a6f882f94b3a97e39a93161f667784
wyllow7 <will94539@gmail.com>
2026-04-27 21:08:38 -0700

    Tweaked score alg again to be less harsh at smaller distances

--- backend/score_algorithm.py
+++ backend/score_algorithm.py
@@ -5,23 +5,21 @@ SpartanGuessr Score Algorithm
     - From "Beginner's Guide to Geoguessr", might be helpful later: https://www.plonkit.net/beginners-guide#game-mechanics
         - "'Distance' naturally refers to the distance between the guess and the correct location. 
            'Max Distance' is the diagonal of the smallest rectangle possible that would contain every location in the map."
+    - Added ^1.5 to distance/max_distance to make it less harsh at shorter distances, accounts for smaller campus size compared to world map
 """
 
 import math
 
 def score_algorithm(guess_point, correct_point):
-    MAX_DISTANCE = 2800 #2073 # diagonal of smallest rectangle possible that contains every location on map, ie. rectangle that surrounds sjsu campus
+    MAX_DISTANCE = 2073 # diagonal of smallest rectangle possible that contains every location on map, ie. rectangle that surrounds sjsu campus
 
     distance = math.dist(guess_point, correct_point) # distance between guess and correct location
-        # previous distance formula in case math function doesnt work, need individual coordinates:
-        # distance = math.sqrt(pow((correct_x - guess_x), 2) + pow((correct_y - guess_y), 2))
 
     # calculate score
-    score = 5000 * pow(math.e, (-10 * (distance / MAX_DISTANCE)))
+    score = 5000 * pow(math.e, (-10 * pow(distance / MAX_DISTANCE, 1.5)))
     if (score > 4950):
         score = 5000
     return round(score), distance
 
 # print(score_algorithm([0,0], [0,0]))
 # ^ just for debugging
-# test
