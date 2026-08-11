83fc1c99aaae05e51a32b073fad5a5f9efda04e4
Paracosms <92565929+Paracosms@users.noreply.github.com>
2026-08-11 15:04:58 -0700

    finish about page

--- src/components/AboutPanel.tsx
+++ src/components/AboutPanel.tsx
@@ -1,4 +1,6 @@
 import Logo from "../assets/SpartanguessrLogo.png";
+import Helmet from "../assets/icons/Helmet.png";
+import Spear from "../assets/icons/WebIcon.png";
 import { Link } from "react-router-dom";
 
 const demoTeam = [
@@ -17,7 +19,7 @@ export default function AboutPanel() {
             aria-labelledby="landing-tab-about"
         >
             <img className="about-logo" src={Logo} alt="SpartanGuessr" />
-            <div className="about-thanks">
+            <div className="about-section">
                 <strong>Special thanks to the original team for helping with the game's demo:</strong>
                 <div className="about-team-list" aria-label="Original demo team">
                     {demoTeam.map((member, index) => (
@@ -28,11 +30,22 @@ export default function AboutPanel() {
                     ))}
                 </div>
             </div>
-            <div className="about-creator">
-                <span>Created by:</span>
-                <strong>Andrew Hiponia</strong>
+            <div className="about-creator-stage">
+                <img className="about-creator-spear" src={Spear} alt="" aria-hidden="true" />
+                <div className="about-creator">
+                    <span>Created by:</span>
+                    <strong>Andrew</strong>
+                </div>
+                <img className="about-creator-helmet" src={Helmet} alt="" aria-hidden="true" />
             </div>
-            <p className="about-submit">Want to add images to the game? Follow the steps <Link to="/map">here</Link>.</p>
+            <div className="about-section about-contribute-section">
+                <strong>Contribute / Feedback</strong>
+            </div>
+            <p className="about-links about-contribute-section">
+                <Link to="/map">Submit Photos</Link>
+                <span aria-hidden="true">·</span>
+                <a href="mailto:andrew.hiponia@sjsu.edu?subject=SpartanGuessr%20Issue">Report an Issue</a>
+            </p>
         </section>
     );
 }
